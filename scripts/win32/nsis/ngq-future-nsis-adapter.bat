7b74bb1e922d743d6abd287a021ab029645dbaaf
alisovenko <alexander.lisovenko@gmail.com>
2015-02-08 19:12:58 +0300

    build gdal 2.0.0

--- scripts/win32/nsis/ngq-future-nsis-adapter.bat
+++ scripts/win32/nsis/ngq-future-nsis-adapter.bat
@@ -11,7 +11,7 @@ makensis.exe ^
 /DSAGA_SRC_DIR="E:\builds\env\osgeo4w-saga" ^
 /DINSTALLER_NAME=%NGQBUILDER_BUILDS_DIR%\%4 ^
 /DPROGRAM_VERSION=%3 ^
-/DPLUGINS="e:\builds\qgis-plugins\identifyplus e:\builds\qgis-plugins\ngwadmin e:\builds\qgis-plugins\publish2ngw e:\builds\qgis-plugins\qtiles e:\builds\qgis-plugins\quick_map_services e:\builds\qgis-plugins\ru_geocoder e:\builds\qgis-plugins\qgis2mobile" ^
+/DPLUGINS="e:\builds\qgis-plugins\identifyplus e:\builds\qgis-plugins\publish2ngw e:\builds\qgis-plugins\qtiles e:\builds\qgis-plugins\quick_map_services e:\builds\qgis-plugins\ru_geocoder e:\builds\qgis-plugins\qgis2mobile E:\builds\qgis-plugins\ngw_connect" ^
 /DQGIS_MANUAL_FILE_NAME_RU="QGIS-2.6-UserGuide-ru.pdf" ^
 /DQGIS_MANUAL_FILE_NAME_EN="QGIS-2.6-UserGuide-en.pdf" ^
 %2
